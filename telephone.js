dbb2a5a5f97a1545c38bda692e672a26e5fc7e19
Jan Korczak <jankorczak@Jans-MacBook-Air.local>
2023-05-14 11:12:07 +0200

    telephone app done, working on contacts app html

--- telephone.js
+++ telephone.js
@@ -3,7 +3,6 @@ const currentNumber = document.querySelector('.current-number')
 const telephoneSvg = document.querySelector('.telephone-svg')
 
 function displayNumbers() {
-    console.log(this.textContent);
     return currentNumber.innerHTML += this.textContent;
 }
 
@@ -19,13 +18,35 @@ function readingTime() {
     return currentTime;
 }
 
+const listOfCalls = document.querySelector('.list-of-calls');
+
+function renderingListOfCalls(data) {
+    const listOfCallsData = JSON.parse(localStorage.getItem("data"));
+    if(listOfCallsData) {
+        const rendered = listOfCallsData.map((caller) => {
+        const div = document.createElement("div");
+        const h3 = document.createElement("h3");
+        const span = document.createElement("span");
+        const phoneNumber = document.createTextNode(`${caller.phonenumber}`);
+        const callTime = document.createTextNode(`${caller.time}`);
+
+        h3.appendChild(phoneNumber);
+        span.appendChild(callTime);
+        div.appendChild(h3);
+        div.appendChild(span);
+        div.classList.add('list-item');
+        listOfCalls.appendChild(div);
+    })
+}
+}
+renderingListOfCalls();
+
 // making the calls save in the localstorage
 
 const data = []
 
 telephoneSvg.addEventListener('click', () => {
     let localStorageData = JSON.parse(localStorage.getItem("data"));
-    console.log(data.length)
     if(currentNumber.textContent) {
         if(data.length == 0 && localStorageData != null) {
             data.unshift({phonenumber: currentNumber.textContent, time: readingTime()}, ...localStorageData);
@@ -35,8 +56,75 @@ telephoneSvg.addEventListener('click', () => {
         }
     }
     localStorage.setItem("data", JSON.stringify(data));
+
+    const div = document.createElement("div");
+    const h3 = document.createElement("h3");
+    const span = document.createElement("span");
+    const phoneNumber = document.createTextNode(currentNumber.textContent);
+    console.log(phoneNumber)
+    const callTime = document.createTextNode(readingTime());
+
+    h3.appendChild(phoneNumber);
+    span.appendChild(callTime);
+    div.appendChild(h3);
+    div.appendChild(span);
+    div.classList.add('list-item');
+    listOfCalls.prepend(div);
+
+    currentNumber.innerHTML = '';
 })
 
 
 
 numberButtons.forEach((button) => button.addEventListener('click', displayNumbers));
+
+const latestCallsLink = document.querySelector('.latest-calls-link');
+
+const bottomSideLatestCalls = document.querySelector('.bottom-side-latest-calls');
+const numbersLink = bottomSideLatestCalls.querySelector('.numbers-link');
+
+const bottomSide = document.querySelector('.bottom-side');
+const telephonePanel = document.querySelector('.telephone-panel');
+const numbers = document.querySelector('.numbers');
+
+const titleAndInput = document.querySelector('.title-and-input');
+
+latestCallsLink.addEventListener("click", () => {
+    
+    telephonePanel.classList.add('disabled');
+    bottomSideLatestCalls.classList.add('active');
+    bottomSide.classList.add('disabled');
+
+    numbers.classList.add('disabled');
+    listOfCalls.classList.remove('disabled');
+    titleAndInput.classList.remove('disabled');
+
+
+})
+
+numbersLink.addEventListener("click", () => {
+    telephonePanel.classList.remove('disabled');
+    bottomSideLatestCalls.classList.remove('active');
+    bottomSide.classList.remove('disabled');
+    numbers.classList.remove('disabled');
+    listOfCalls.classList.add('disabled');
+    titleAndInput.classList.add('disabled');
+})
+
+
+// making input search
+
+const searchInput = document.querySelector('.input-search');
+
+searchInput.addEventListener('input', (e) => {
+    const listItem = listOfCalls.querySelectorAll('.list-item');
+    listItem.forEach((item) => {
+        const phoneNumber = item.querySelector('h3').textContent;
+        if(phoneNumber.includes(e.target.value)) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
+        }
+    }
+    )
+})
